bd65a24b699cbc9eda1c587160c1cfe3e2db6b97
nixval <nicovaliantoku@gmail.com>
2026-02-18 00:33:48 +0700

    refactor(backends): split config tests into module file

--- src/backends/config.rs
+++ src/backends/config.rs
@@ -291,25 +291,4 @@ impl Default for BackendConfig {
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_binary_specifier_single() {
-        let bin = BinarySpecifier::Single("ls".to_string());
-        assert_eq!(bin.primary(), "ls");
-    }
-
-    #[test]
-    fn test_binary_specifier_multiple() {
-        let bin = BinarySpecifier::Multiple(vec!["paru".to_string(), "yay".to_string()]);
-        assert_eq!(bin.primary(), "paru");
-    }
-
-    #[test]
-    fn test_backend_config_default() {
-        let config = BackendConfig::default();
-        assert_eq!(config.name, "unknown");
-        assert_eq!(config.list_format, OutputFormat::SplitWhitespace);
-    }
-}
+mod tests;
