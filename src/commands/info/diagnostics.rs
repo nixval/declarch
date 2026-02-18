c3b33b72c9a8ccef8b27bc38eb51c9a952454316
nixval <nicovaliantoku@gmail.com>
2026-02-18 11:28:47 +0700

    refactor(info): extract diagnostics update notice

--- src/commands/info/diagnostics.rs
+++ src/commands/info/diagnostics.rs
@@ -4,11 +4,12 @@ use crate::project_identity;
 use crate::state;
 use crate::ui as output;
 use crate::utils::paths;
-use crate::utils::update_check::{InstallOwner, is_managed_by_package_manager, update_hint_cached};
 use checks::check_backends_dynamically;
 pub(super) use checks::collect_state_signature_duplicates;
+pub(super) use update_notice::maybe_print_update_notification;
 
 mod checks;
+mod update_notice;
 
 pub(super) fn run_doctor(verbose: bool) -> Result<()> {
     output::header("System Diagnosis");
@@ -200,46 +201,4 @@ pub(super) fn run_doctor(verbose: bool) -> Result<()> {
     maybe_print_update_notification();
 
     Ok(())
-}
-
-pub(super) fn maybe_print_update_notification() {
-    let Some(hint) = update_hint_cached() else {
-        return;
-    };
-
-    output::separator();
-    output::warning(&format!(
-        "New {} release available: {} -> {}",
-        project_identity::BINARY_NAME,
-        hint.current,
-        hint.latest
-    ));
-
-    if is_managed_by_package_manager(&hint.owner) {
-        let msg = match hint.owner {
-            InstallOwner::Pacman => format!(
-                "Update using package manager: paru -Syu {}",
-                project_identity::BINARY_NAME
-            ),
-            InstallOwner::Homebrew => format!(
-                "Update using package manager: brew upgrade {}",
-                project_identity::BINARY_NAME
-            ),
-            InstallOwner::Scoop => format!(
-                "Update using package manager: scoop update {}",
-                project_identity::BINARY_NAME
-            ),
-            InstallOwner::Winget => format!(
-                "Update using package manager: winget upgrade {}",
-                project_identity::BINARY_NAME
-            ),
-            _ => "Update using your package manager".to_string(),
-        };
-        output::info(&msg);
-    } else {
-        output::info(&format!(
-            "For script/manual install, run: {} self-update",
-            project_identity::BINARY_NAME
-        ));
-    }
 }
